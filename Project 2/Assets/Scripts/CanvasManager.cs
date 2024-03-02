7ed34d6af6bb5379c729c77315bfb8bc365f571d
Joseph O'Keeffe <josephdokeeffe@yahoo.ie>
2024-03-02 17:49:03 +0000

    Done the first effect - Increase in size then decrease

--- Project 2/Assets/Scripts/CanvasManager.cs
+++ Project 2/Assets/Scripts/CanvasManager.cs
@@ -1,60 +1,49 @@
-using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class CanvasManager : MonoBehaviour
 {
-    public Sprite backgroundSprite; // Reference to your sprite, assign it in the Unity Editor
+    public Sprite backgroundSprite;
     private Canvas canvas;
 
-    class ImageScalar : MonoBehaviour
+    class ObjectToScale : MonoBehaviour
     {
         public RectTransform rectTransform { get; set; }
         public Vector2 targetSize { get; set; }
         public float speed { get; set; }
-        public bool done { get; set; }
+        public bool isMaxSize { get; set; }
+        public bool isFinalSize { get; set; }
     }
 
-    List<ImageScalar> imagesToScale = new List<ImageScalar>();
+    List<ObjectToScale> objectsToScale = new List<ObjectToScale>();
 
     void Start()
     {
-
         CreateCanvas();
-        CreateBackgroundImage();
+        CreateRectangleOnCanvas(Vector2.zero, Vector2.one, Vector2.zero, Vector2.zero, Color.red);
+        CreateImageOnCanvas("Player", 1, Vector2.zero, 5f, 1);
+        CreateText("AndyName", "Welcome to Entropy!", 10, Color.green, Vector2.zero, 5, 1.5f);
+    }
 
-        GameObject textObject = new GameObject("TextMeshProText");
-        TextMeshProUGUI textMeshProUGUI = textObject.AddComponent<TextMeshProUGUI>();
+    void Update()
+    {
+        IncreaseObjectSize();
 
-        // Set text properties
-        textMeshProUGUI.text = "Hello, TextMeshPro!";
-        textMeshProUGUI.fontSize = 24;
-        textMeshProUGUI.color = Color.white;
-
-        // Set the parent of the text to the canvas
-        textObject.transform.SetParent(canvas.transform, false);
-
-        // Optionally, you can set the RectTransform properties for the text
-        RectTransform textRectTransform = textMeshProUGUI.GetComponent<RectTransform>();
-        textRectTransform.anchoredPosition = new Vector2(0.5f, 0.5f); // Center of the canvas
+        foreach(ObjectToScale temp in objectsToScale)
+        {
+            if(temp.isMaxSize)
+            {
+                DecreaseObjectSize(temp);
+            }
+        }
     }
 
     void CreateCanvas()
     {
         canvas = new GameObject("Canvas").AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-    }
-
-    void CreateBackgroundImage()
-    {
-        
-        CreateRectangleOnCanvas(Vector2.zero, Vector2.one, Vector2.zero, Vector2.zero, Color.red);
-        CreateImageOnCanvas("Player", Vector2.one * 3f, 10f);
     }
 
     void CreateRectangleOnCanvas(Vector2 anchorMin, Vector2 anchorMax, Vector2 anchoredPosition, Vector2 sizeDelta, Color colour)
@@ -71,58 +60,91 @@ public class CanvasManager : MonoBehaviour
         rectTransform.anchorMax = anchorMax;
         rectTransform.anchoredPosition = anchoredPosition;
         rectTransform.sizeDelta = sizeDelta;
-        
+
     }
-    void CreateImageOnCanvas(string spriteName, Vector2 targetScale, float speed)
+    void CreateImageOnCanvas(string spriteName, float startScale, Vector2 startPos, float targetScale, float scaleSpeed)
     {
         GameObject backgroundImage = new GameObject(spriteName + "Image");
         Image newImage = backgroundImage.AddComponent<Image>();
-
-        // Load the sprite by name
         Sprite loadedSprite = Resources.Load<Sprite>(spriteName);
 
-        if (loadedSprite != null)
-        {
-            newImage.sprite = loadedSprite;
-        }
-        else
-        {
-            Debug.LogError("Sprite not found with name: " + spriteName);
-        }
+        newImage.sprite = loadedSprite;
 
         backgroundImage.transform.SetParent(canvas.transform, false);
 
         RectTransform rectTransform = newImage.GetComponent<RectTransform>();
-        rectTransform.localScale = Vector2.one;
-        rectTransform.localPosition = Vector2.one;
+        rectTransform.localScale = new Vector2(startScale, startScale);
+        rectTransform.localPosition = startPos;
 
-
-        ImageScalar imgscalar = new ImageScalar();
-        imgscalar.done = false;
-        imgscalar.targetSize = targetScale;
-        imgscalar.speed = speed;
-        imgscalar.rectTransform = rectTransform;
-        imagesToScale.Add(imgscalar);
-
-
+        CreateObjectToScale(new Vector2(targetScale, targetScale), scaleSpeed, rectTransform);
     }
 
-    void Update()
+    void CreateText(string textName, string textString, int textSize, Color colour, Vector2 startPos, float targetScale, float scaleSpeed)
     {
-        if (imagesToScale.Count > 0)
+        GameObject textObject = new GameObject(textName);
+        TextMeshProUGUI textMeshProUGUI = textObject.AddComponent<TextMeshProUGUI>();
+
+        textMeshProUGUI.text = textString;
+        textMeshProUGUI.fontSize = textSize;
+        textMeshProUGUI.color = colour;
+        textMeshProUGUI.fontStyle = FontStyles.Bold;
+
+        textObject.transform.SetParent(canvas.transform, false);
+
+        RectTransform textRectTransform = textMeshProUGUI.GetComponent<RectTransform>();
+        //textRectTransform.anchoredPosition = new Vector2(0.5f, 0.5f);
+        textRectTransform.sizeDelta = new Vector2(textMeshProUGUI.preferredWidth, textSize); // Set width based on preferred width
+        textRectTransform.localPosition = new Vector2(startPos.x, startPos.y);
+
+        CreateObjectToScale(new Vector2(targetScale, targetScale), scaleSpeed, textRectTransform);
+    }
+
+    void CreateObjectToScale(Vector2 targetSize, float speed, RectTransform rectTransform)
+    {
+        ObjectToScale objectToScale = new ObjectToScale();
+        objectToScale.isMaxSize = false;
+        objectToScale.isFinalSize = false;
+        objectToScale.targetSize = targetSize;
+        objectToScale.speed = speed;
+        objectToScale.rectTransform = rectTransform;
+        objectsToScale.Add(objectToScale);
+    }
+
+    void IncreaseObjectSize()
+    {
+        if (objectsToScale.Count > 0)
         {
-            for (int i = 0; i < imagesToScale.Count; i++)
+            for (int i = 0; i < objectsToScale.Count; i++)
             {
-                if (imagesToScale[i].done)
+                if (objectsToScale[i].isMaxSize)
                 {
                     return;
                 }
-                imagesToScale[i].rectTransform.localScale += Vector3.one * Time.deltaTime * imagesToScale[i].speed;
-                if (imagesToScale[i].rectTransform.localScale.x > imagesToScale[i].targetSize.x)
+
+                objectsToScale[i].rectTransform.localScale += Vector3.one * Time.deltaTime * objectsToScale[i].speed;
+                if (objectsToScale[i].rectTransform.localScale.x > objectsToScale[i].targetSize.x)
                 {
-                    imagesToScale[i].done = true;
-                    imagesToScale[i].rectTransform.localScale = imagesToScale[i].targetSize;
+                    objectsToScale[i].isMaxSize = true;
+                    objectsToScale[i].rectTransform.localScale = objectsToScale[i].targetSize;
                 }
+            }
+        }
+    }
+
+    void DecreaseObjectSize(ObjectToScale temp)
+    {
+        if (objectsToScale.Count > 0)
+        {
+            if (temp.isFinalSize)
+            {
+                return;
+            }
+
+            temp.rectTransform.localScale -= Vector3.one * Time.deltaTime * temp.speed;
+            if (temp.rectTransform.localScale.x < temp.targetSize.x * 0.8f)
+            {
+                temp.isFinalSize = true;
+                temp.rectTransform.localScale = temp.targetSize * 0.8f;
             }
         }
     }
